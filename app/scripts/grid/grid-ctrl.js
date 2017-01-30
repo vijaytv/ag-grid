23f51054d425040025842e5030fbf41e67ecc330
Vijay <vijay_tv@yahoo.com>
2017-01-29 19:00:11 -0500

    next

--- app/scripts/grid/grid-ctrl.js
+++ app/scripts/grid/grid-ctrl.js
@@ -5,8 +5,6 @@ var agGrid = require('ag-grid');
 agGrid.initialiseAgGridWithAngular1(angular);
 
 var GridCtrl = function($scope,DataService) {
-  $scope.testVar = 'We are up and running from a required module!';
-
   var columnDefs = [
           {headerName: "City", field: "city"},
           {headerName: "DOB", field: "dob"},
@@ -20,7 +18,7 @@ var GridCtrl = function($scope,DataService) {
 
   DataService.getPeople().then(function(data){
       console.log(data);
-      $scope.gridOptions.rowData = data;
+      $scope.gridOptions.api.setRowData(data);
   })
 
 };
